a272989966e2844bbb701b73d4b84273603ae461
francesco <francesco@Mordor>
2020-10-28 11:50:09 +0100

    Bugfixing

---
+++ src/main/java/it/unical/it/primeNumbers/PrimeFinder.java
@@ -0,0 +1,59 @@
+package it.unical.it.primeNumbers;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeFinder 
+{
+	private boolean isPrime(Integer n)
+	{
+		for(int i = 2; i <= Math.sqrt(n); i++)
+		{
+			if(n % i == 0) return false;
+		}
+		
+		return false;
+	}
+	
+	public ArrayList<Integer> getPrimes(Integer n)
+	{
+		if(n == 1 || n == 0) return null;
+		
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		if(n >= 2)
+			primes.add(2);
+		
+		for(int i = 3; i <= n; i+=2)
+		{
+			if(isPrime(i))
+				primes.add(i);
+		}
+		
+		return primes;
+	}
+	
+    public static void main( String[] args )
+    {
+        PrimeFinder finder = new PrimeFinder();
+        
+        Integer n = 10;
+        ArrayList<Integer> primes = finder.getPrimes(n);
+        
+        if(primes == null)
+        {
+        	System.out.println("Non ci sono primi");
+        }
+        else
+        {
+        	for(Integer i : primes)
+            {
+            	System.out.println(i);
+            }
+        }
+        
+        
+    }
+}
